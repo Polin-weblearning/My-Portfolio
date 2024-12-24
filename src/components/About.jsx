4cb81ef069313bae598c64531daa941cbe1e1321
Pinlo-Eror-code <athpolin2021@gmail.com>
2024-12-25 01:58:12 +0700

    fefef

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import aboutImg from "../assets/about1.jpg";
+import aboutImg from "../assets/aboutimg2.jpg";
 import { ABOUT_TEXT } from "../constants";
 import { motion } from "framer-motion";
 
@@ -15,7 +15,7 @@ const About = () => {
           whileInView={{ opacity: 1, x: 0 }}
           initial={{ opacity: 0, x: -100 }}
           transition={{ duration: 0.5 }}
-          className="w-full lg:w-1/2 lg:p-8"
+          className="w-full lg:w-1/3 lg:p-8"
         >
           <div className="flex items-center justify-center">
             <img className="rounded-2xl" src={aboutImg} alt="about" />
