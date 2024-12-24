fdf1621ab177f4c2c98100885fc8e028f4f0330a
Pinlo-Eror-code <athpolin2021@gmail.com>
2024-12-25 01:55:30 +0700

    fefe

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,4 @@
 import aboutImg from "../assets/about1.jpg";
-import aboutImg2 from "../assets/aboutimg2.jpg";
 import { ABOUT_TEXT } from "../constants";
 import { motion } from "framer-motion";
 
@@ -33,29 +32,7 @@ const About = () => {
           </div>
         </motion.div>
       </div>
-      <div className="flex flex-wrap">
-      <motion.div
-          whileInView={{ opacity: 1, x: 0 }}
-          initial={{ opacity: 0, x: 100 }}
-          transition={{ duration: 0.5 }}
-          className="w-full lg:w-1/2"
-        >
-          <div className="flex justify-center lg:justify-start">
-            <p className="my-2 max-w-xl  py-6">{ABOUT_TEXT}</p>
-          </div>
-        </motion.div>
-        <motion.div
-          whileInView={{ opacity: 1, x: 0 }}
-          initial={{ opacity: 0, x: -100 }}
-          transition={{ duration: 0.5 }}
-          className="w-full lg:w-1/2 lg:p-8"
-        >
-          <div className="flex items-center justify-center">
-            <img className="rounded-2xl" src={aboutImg2} alt="about" />
-          </div>
-        </motion.div>
-       
-      </div>
+      
     </div>
   );
 };
