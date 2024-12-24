9495ccb8eee2faa6a3872b73aef291379f991e3f
Pinlo-Eror-code <athpolin2021@gmail.com>
2024-12-25 01:28:35 +0700

    dfdf

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,5 @@
-import aboutImg from "../assets/about.jpg";
+import aboutImg from "../assets/about1.jpg";
+import aboutImg2 from "../assets/aboutimg2.jpg";
 import { ABOUT_TEXT } from "../constants";
 import { motion } from "framer-motion";
 
@@ -31,6 +32,29 @@ const About = () => {
             <p className="my-2 max-w-xl  py-6">{ABOUT_TEXT}</p>
           </div>
         </motion.div>
+      </div>
+      <div className="flex flex-wrap">
+      <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 0.5 }}
+          className="w-full lg:w-1/2"
+        >
+          <div className="flex justify-center lg:justify-start">
+            <p className="my-2 max-w-xl  py-6">{ABOUT_TEXT}</p>
+          </div>
+        </motion.div>
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.5 }}
+          className="w-full lg:w-1/2 lg:p-8"
+        >
+          <div className="flex items-center justify-center">
+            <img className="rounded-2xl" src={aboutImg2} alt="about" />
+          </div>
+        </motion.div>
+       
       </div>
     </div>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,65 +39,68 @@ const Hero = () => {
 
   return (
     <>
-    <motion.div
-      initial="hidden"
-      animate="visible"
-      className="border-neutral-900 pb-4 lg:mb-35"
-    >
-      <div className="flex flex-wrap">
-        <div className="w-full lg:w-1/2">
-          <div className="flex flex-col items-center lg:items-start">
-            <motion.h1
-              variants={container(0)}
-              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
-            >
-              Ath Polin
-            </motion.h1>
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        className="border-neutral-900 pb-4 lg:mb-35"
+      >
+        <div className="flex flex-wrap">
+          <div className="w-full lg:w-1/2">
+            <div className="flex flex-col items-center lg:items-start">
+              <motion.h1
+                variants={container(0)}
+                className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
+              >
+                Ath Polin
+              </motion.h1>
 
-            <motion.span
-              key={isDeveloper ? "developer" : "designer"}
-              initial={{ opacity: 0, y: -50 }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: -50 }}
-              transition={{ duration: 1 }}
-              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"
-            >
-              {isDeveloper ? "Front-End Developer" : "UX/UI Designer"}
-            </motion.span>
+              <motion.span
+                key={isDeveloper ? "developer" : "designer"}
+                initial={{ opacity: 0, y: -50 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -50 }}
+                transition={{ duration: 1 }}
+                className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"
+              >
+                {isDeveloper ? "Front-End Developer" : "UX/UI Designer"}
+              </motion.span>
 
-            <motion.p
+              <motion.p
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: -100 }}
+                transition={{ duration: 1 }}
+                className="my-2 max-w-xl py-6"
+              >
+                {HERO_CONTENT}
+              </motion.p>
+            </div>
+          </div>
+          <div className="w-full lg:w-1/2 lg:p-8">
+            <motion.div
               whileInView={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: -100 }}
+              initial={{ opacity: 0, x: 100 }}
               transition={{ duration: 1 }}
-              className="my-2 max-w-xl py-6"
+              className="flex justify-center"
             >
-              {HERO_CONTENT}
-            </motion.p>
+              <img
+                src={profilePic}
+                alt="ath polin"
+                className="cursor-pointer"
+                onClick={togglePlayPause} // Toggle audio on image click audio play
+              />
+            </motion.div>
+            <motion.h1
+              whileInView={{ opacity: 1, y: 0 }}
+              initial={{ opacity: 0, y: 100 }}
+              transition={{ duration: 0.5 }}
+              className="bebas-neue-regular  my-2 text-center text-xl bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl  text-transparent"
+            >
+              "Click the image, and make sure your volume is up to hear the
+              magic!"
+            </motion.h1>
           </div>
         </div>
-        <div className="w-full lg:w-1/2 lg:p-8">
-          <motion.div
-            whileInView={{ opacity: 1, x: 0 }}
-            initial={{ opacity: 0, x: 100 }}
-            transition={{ duration: 1 }}
-            className="flex justify-center"
-          >
-            <img
-              src={profilePic}
-              alt="ath polin"
-              className="cursor-pointer"
-              onClick={togglePlayPause} // Toggle audio on image click audio play
-            />
-          </motion.div>
-          <motion.h1
-      whileInView={{opacity: 1 , y:0}}
-      initial={{ opacity: 0, y: 100 }}
-      transition={{duration:0.5}}
-      className="bebas-neue-regular  my-2 text-center text-xl bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl  text-transparent">"Click the image, and make sure your volume is up to hear the magic!"</motion.h1>
-        </div>
-      </div>
-    </motion.div>
-    
+      </motion.div>
     </>
   );
 };
